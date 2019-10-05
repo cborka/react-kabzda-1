376470ec11853d872cb9831c6827fb006889f870
Boris Ts. (cborka) <cborka777@yandex.ru>
2019-10-05 15:16:51 +0700

    2019-10-05 изучение реакт

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Technologies />
+        </div>
+    );
+};
+const Header = () => {
+    return (
+        <div>
+            <a href="#s">Home </a>
+            <a href="#s">News </a>
+            <a href="#s">Messages</a>
+        </div>
+    );
+};
+const Technologies = () => {
+    return (
+        <div>
+            <div className="App">
+                <ul>
+                    <li>css</li>
+                    <li>html</li>
+                    <li>js</li>
+                    <li>react</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
 
 export default App;
