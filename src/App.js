0fb55dc9d29601b4fec9541c724dedac14b18605
Boris Ts. (cborka) <cborka777@yandex.ru>
2019-10-06 22:18:16 +0700

    2019-10-06 изучение реакт

--- src/App.js
+++ src/App.js
@@ -3,32 +3,52 @@ import './App.css';
 
 const App = () => {
     return (
-        <div>
-            <Header />
-            <Technologies />
-        </div>
-    );
-};
-const Header = () => {
-    return (
-        <div>
-            <a href="#s">Home </a>
-            <a href="#s">News </a>
-            <a href="#s">Messages</a>
-        </div>
-    );
-};
-const Technologies = () => {
-    return (
-        <div>
-            <div className="App">
-                <ul>
-                    <li>css</li>
-                    <li>html</li>
-                    <li>js</li>
-                    <li>react</li>
-                </ul>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='http://mp.metapoznanie.ru/img/mp21.bmp'></img>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a href=''>Profile</a>
+                </div>
+                <div>
+                    <a href=''>Messages</a>
+                </div>
+                <div>
+                    <a href=''>News</a>
+                </div>
+                <div>
+                    <a href=''>Music</a>
+                </div>
+                <div>
+                    <a href=''>Settings</a>
+                </div>
+                <div>
+                    <a href=''>Profile</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                <img src='https://cdn.pixabay.com/photo/2019/10/02/00/41/lake-4519623_960_720.jpg'></img>
+                </div>
+                <div>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5T9mDfy0eWJY8jKLwofob7cPJiKGDLGEy_hJPdVGRki0Y5PMa6g'></img>
+                </div>
+                <div>
+                    My post
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        New post 1
+                    </div>
+                    <div>
+                        New post 2
+                    </div>
+                </div>
+
             </div>
+            <footer className='footer'></footer>
         </div>
     );
 };
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            it-kamasutra.com footer
+        </div>
+    );
+};
+
+export default Footer;
